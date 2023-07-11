f901267ef2f30ab3d000ccf6505eb77eb3fe927d
Harish Choure <harishchoureoutlook@gmail.com>
2023-07-11 23:41:58 +0530

    Added js and html file

---
+++ dom_js_basic/index.js
@@ -0,0 +1,44 @@
+// const body  = document.body
+// body.append("Hello harry")//Append for both string and container
+// //body.appendChild("div")//Append only container
+
+
+// const div = document.createElement("div"); // Create a <div> element
+// //div.innerText = 'Hello world'; // Set the text content of the <div> element
+// div.textContent  = 'Hello harish' // Set the text content of the <div> element
+// body.append(div); // Append the <div> element to the body
+
+
+
+// const div = document.querySelector('div')
+
+// console.log(div.textContent)//It display all content of the container
+// console.log(div.innerText)//It does not show display none data
+
+
+// const body = document.body
+// const div = document.createElement("div")
+// div.innerHTML=`<strong>Hare Krishna </strong>`
+
+// body.append(div)
+
+
+
+
+const div = document.querySelector('div')
+const spanhi = document.querySelector('.hi')
+const spanby = document.querySelector('#bye')
+
+// spanhi.remove()
+// div.append(spanhi)
+// div.removeChild(spanby)
+
+// console.log(spanby.getAttribute('id'));
+// console.log(spanby.id)
+// console.log(spanby.setAttribute("id","ddss"))
+
+
+// spanby.classList.toggle('h1', false)
+// spanby.classList.add('new-cls')
+
+spanby.style.color = 'red'
